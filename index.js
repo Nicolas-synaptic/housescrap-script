8fb2530779acb3c677320ca448798554786ab647
Nicolas Cofre <nicolas.cofre@practicante1.local>
2023-01-05 22:13:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get("/",async (req, res) => {
+
+    const browser = await puppeteer.launch({
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    })
+    const page = await browser.newPage()
+    await page.goto('https://www.portalinmobiliario.com/arriendo/casa/rancagua-bernardo-ohiggins');
+
+    const data = await page.evaluate( () => {
+        const $rents = document.querySelectorAll('.shops__layout-item')
+        const data = []
+        $rents.forEach(($rent) => {
+
+           data.push({
+              price : $rent.querySelector('.price-tag-fraction').textContent,
+              size  : $rent.querySelector('.ui-search-card-attributes__attribute:nth-child(1)').textContent,
+              rooms : ($rent.querySelector('.ui-search-card-attributes__attribute:nth-child(2)') ? $rent.querySelector('.ui-search-card-attributes__attribute:nth-child(2)').textContent : " " ),
+              place : $rent.querySelector('.ui-search-item__location').textContent,
+           })
+        })
+        return {
+           rents: data,
+        }
+     })
+
+     res.send(data)
+
+    await browser.close()
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
